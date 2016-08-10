21eba2fdead1c1ee2e96692ed4ad422260ae6159
hirofumimukuo <the.end.of.universe@gmail.com>
2016-08-10 20:59:11 +0900

    Moved

--- change_type.sql
+++
@@ -1,7 +0,0 @@
-BEGIN
-
---hogehoge:column name
-
-ALTER TABLE tbl_name ALTER COLUMN hogehoge TYPE integer USING hogehoge::integer;
-
-COMMIT;
--- inport_csv.sql
+++
@@ -1,7 +0,0 @@
-BEGIN
-
---csvfile:c:\tmp\tbl2.csv
-
-copy tbl_name from E'd:\\tbl2.csv' with csv;
-
-COMMIT;
