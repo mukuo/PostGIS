18696df06b2e11465ca0d6d6296d65a1be0badb0
Hirofumi Mukuo <the.end.of.universe@gmail.com>
2019-05-17 01:11:51 +0900

    インデント修正

--- point_to_line.sql
+++ point_to_line.sql
@@ -2,8 +2,12 @@
 --'id' woudl be changable
 
 SELECT id, ST_MakeLine(ordered_gps_table_name.the_geom) AS track_line
-    FROM (
-	SELECT id,"datetime", the_geom FROM gps_org_table_name ORDER BY id, "datetime") as ordered_gps_table_name
-    		GROUP BY ordered_gps_table_name.id
-    		ORDER BY ordered_gps_table_name.id
+FROM
+	(
+	SELECT id,"datetime", the_geom
+	FROM gps_org_table_name
+	ORDER BY id, "datetime"
+	) AS ordered_gps_table_name
+GROUP BY ordered_gps_table_name.id
+ORDER BY ordered_gps_table_name.id
 ;
